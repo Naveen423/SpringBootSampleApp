8e10d9afaf552391b6d893f5d162c795520fb3c8
Naveen423 <naveenkunkulla9@gmail.com>
2021-10-15 13:09:06 +0530

    WH-4182 Update Runner with Logging using SL4J

--- src/main/java/init/nareshit/raghu/runner/Runner.java
+++ src/main/java/init/nareshit/raghu/runner/Runner.java
@@ -1,17 +1,24 @@
 package init.nareshit.raghu.runner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Runner implements CommandLineRunner {
-
+    private static Logger logger = LoggerFactory.getLogger(Runner.class);
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
         int a=10;
+        try {
+        logger.info("STARTED");
 		System.out.println("Welcome to Spring Boot Starter Project!"+a);
-      
+        }catch(Exception e) {
+        	e.printStackTrace();
+        }
+        logger.info("END");
 	}
 
 }
