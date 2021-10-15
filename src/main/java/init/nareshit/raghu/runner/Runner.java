8f06f09ef79fc9ddb5a8e1e8da514fe307383734
Naveen423 <naveenkunkulla9@gmail.com>
2021-10-15 11:54:05 +0530

    WH-8563-First Commit

---
+++ src/main/java/init/nareshit/raghu/runner/Runner.java
@@ -0,0 +1,15 @@
+package init.nareshit.raghu.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Runner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+       System.out.println("Welcome to Spring Boot Starter Project!");
+	}
+
+}
---
+++ src/test/java/init/nareshit/raghu/SpringBootSampleAppApplicationTests.java
@@ -0,0 +1,13 @@
+package init.nareshit.raghu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSampleAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
